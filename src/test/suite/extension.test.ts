ab8cb1f1c3994b3a9441692dabc5064584664cf4
Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>
2022-01-06 21:30:23 +0330

    ⚗️ Update tests
    Signed-off-by: Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,8 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
+import * as os from "os";
+import * as fs from "fs";
+import * as path from "path";
 
 suite("Extension", () => {
 	vscode.window.showInformationMessage("Start all tests.");
@@ -10,7 +13,7 @@ suite("Extension", () => {
 			.update(
 				"vscode-send-to-terminal.typeInEcho",
 				value,
-				vscode.ConfigurationTarget.Global,
+				vscode.ConfigurationTarget.Workspace,
 			);
 	}
 
@@ -20,13 +23,45 @@ suite("Extension", () => {
 			.update(
 				"vscode-send-to-terminal.singleOrMergedValueVariableName",
 				value,
-				vscode.ConfigurationTarget.Global,
+				vscode.ConfigurationTarget.Workspace,
 			);
 	}
 
+	async function updateIndexedSelectionVariableNameFormat(value: string) {
+		return vscode.workspace
+			.getConfiguration()
+			.update(
+				"vscode-send-to-terminal.indexedSelectionVariableNameFormat",
+				value,
+				vscode.ConfigurationTarget.Workspace,
+			);
+	}
+
+	async function makeTemporaryWorkspace() {
+		const tempDir = fs.mkdtempSync(
+			path.join(os.tmpdir(), "vscode-send-to-terminal-test-"),
+		);
+		const ws: vscode.WorkspaceFolder = {
+			index: 0,
+			name: "Test workspace",
+			uri: vscode.Uri.file(tempDir),
+		};
+		const result = new Promise((resolve, reject) => {
+			vscode.workspace.onDidChangeWorkspaceFolders((e) => {
+				if (e.added.length) {
+					resolve(tempDir);
+				}
+			});
+		});
+		assert(vscode.workspace.updateWorkspaceFolders(0, undefined, ws));
+		return result;
+	}
+
 	test("sendActiveFileToTerminal()", async () => {
+		const tempDir = await makeTemporaryWorkspace();
 		await updateTypeInEcho(false);
 		await updateSingleOrMergedValueVariableName("SOME_KEY_FOR_VALUE");
+		await updateIndexedSelectionVariableNameFormat("SOME_INDEXED_FORMAT_{0}");
 		const doc = await vscode.workspace.openTextDocument({
 			content: "Hello World!",
 		});
@@ -41,5 +76,7 @@ suite("Extension", () => {
 		assert("env" in co && co.env);
 		assert("SOME_KEY_FOR_VALUE" in co.env);
 		assert.strictEqual(co.env["SOME_KEY_FOR_VALUE"], "Hello World!");
+		assert("SOME_INDEXED_FORMAT_0" in co.env);
+		assert.strictEqual(co.env["SOME_INDEXED_FORMAT_0"], "Hello World!");
 	});
 });
